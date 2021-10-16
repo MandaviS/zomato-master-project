5792dd2c8e0de3617824d32311acb911be76ce2a
Mandavi Srivastava <mandavisrivastava10@gmail.com>
2021-10-16 16:47:34 +0530

    restaurant api

---
+++ server/API/Food/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import passport from "passport"; 
+
+//database models
+import { FoodModel } from "../../database/allModels";
+
+const Router = express.Router();
+
+export default Router;
--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import googleAuthConfig from "./config/google.config";
 
 //microservices routes
 import Auth from "./API/Auth";
+import Restaurant from "./API/Restaurant";
 
 //database connection
 import ConnectDB from "./database/connection";
@@ -30,6 +31,7 @@ googleAuthConfig(passport);
 
 //application routes
 zomato.use("/auth", Auth);
+zomato.use("/restaurant", Restaurant);
 
 zomato.get("/",(req,res) => res.json({ message: "Setup success"}));
 
